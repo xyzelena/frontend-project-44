7630dd7ccb7e5ec6510252a2eeb43332c1fd1b61
xyzelena <emkhuzhina@gmail.com>
2023-01-26 23:34:21 +0530

    Fix oddEvenNumGame.js

--- src/games/oddEvenNumGame.js
+++ src/games/oddEvenNumGame.js
@@ -1,5 +1,5 @@
 import greetingsUser from './cli.js';
-import {playGame} from '../index.js';
+import { playGame } from '../index.js';
 
 const userName = greetingsUser();
 
@@ -7,18 +7,19 @@ const oddEvenNumGame = () => {
   const yes = 'yes';
   const no = 'no';
   console.log(`Answer "${yes}" if the number is even, otherwise answer "${no}".`);
-  
-  let i  = 0; 
 
-  do{
-  const question = Math.floor(Math.random() * 100);
-  const trueAnswer = (question % 2 === 0) ? yes : no;   
-  const result = playGame (userName, question, trueAnswer); 
-  
-  if(result) i += 1;
-  else return false;   
-}while(i<3);
-console.log(`Congratulations, ${userName}!`);  
+  let i = 0;
+
+  do {
+    const question = Math.floor(Math.random() * 100);
+    const trueAnswer = (question % 2 === 0) ? yes : no;
+    const result = playGame(userName, question, trueAnswer);
+
+    if (result) i += 1;
+    else return false;
+  } while (i < 3);
+  console.log(`Congratulations, ${userName}!`);
+return true;
 };
 
 export default oddEvenNumGame;
