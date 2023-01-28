81e6bc8c21e41f28e135f18ececfd206d5cd8697
xyzelena <emkhuzhina@gmail.com>
2023-01-28 12:47:52 +0530

    Fix oddEvenNumGame.js

--- src/games/oddEvenNumGame.js
+++ src/games/oddEvenNumGame.js
@@ -8,12 +8,11 @@ const no = 'no';
 console.log(`Answer "${yes}" if the number is even, otherwise answer "${no}".`);
 
 const oddEvenNumGame = () => {
-
   const question = Math.floor(Math.random() * 101);
   const trueAnswer = (question % 2 === 0) ? yes : no;
 
-  const arrQuestionAnswer = [question,trueAnswer]; 
-  
+  const arrQuestionAnswer = [question, trueAnswer];
+
   return arrQuestionAnswer;
 };
 
