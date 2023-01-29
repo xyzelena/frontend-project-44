834185f979adb5fe51a442dcd9e45d8a5d06c7b9
xyzelena <emkhuzhina@gmail.com>
2023-01-30 00:26:32 +0530

    Fix progressionGame.js

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -5,27 +5,26 @@ const userName = greetingsUser();
 
 console.log('What number is missing in the progression?');
 
-const getRandom = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min);
-};
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-const getSkipValue = (array) => {
-  const skipIndexValue = getRandom(0, array.length - 1);
-  const trueAnswer = array[skipIndexValue];
-  array[skipIndexValue] = '..';
-  return [array, trueAnswer]; 
+const getSkipValue = (arrProgression) => {
+  const arr = arrProgression.slice(0);
+  const skipIndexValue = getRandom(0, arr.length - 1);
+  const trueAnswer = arr[skipIndexValue];
+  arr[skipIndexValue] = '..';
+  return [arr, trueAnswer];
 };
 
 const progressionGame = () => {
-  const firstNum = getRandom(0,5);
-  const step = getRandom(2,5);
-  const lengthProgression = getRandom(5,10);
-  
+  const firstNum = getRandom(0, 5);
+  const step = getRandom(2, 5);
+  const lengthProgression = getRandom(5, 10);
+
   const progression = [firstNum];
 
-  for(let i = 1; i < lengthProgression; i += 1){
-     progression[i] = progression[i-1] + step;
-}
+  for (let i = 1; i < lengthProgression; i += 1) {
+    progression[i] = progression[i - 1] + step;
+  }
 
   const skip = getSkipValue(progression);
 
