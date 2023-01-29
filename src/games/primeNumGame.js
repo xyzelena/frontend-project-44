d5e74ff5b526d882b464e9b0fc56f91494e15495
xyzelena <emkhuzhina@gmail.com>
2023-01-30 02:18:31 +0530

    Fix primeNumGame.js with eslint

--- src/games/primeNumGame.js
+++ src/games/primeNumGame.js
@@ -8,21 +8,21 @@ const no = 'no';
 console.log(`Answer "${yes}" if given number is prime. Otherwise answer  "${no}".`);
 
 const getAnswer = (question) => {
-  if(question <= 1) return no;
-  else if(question === 2) return yes; 
-  else {
-    for(let i = 2; i < question; i += 1){
-       if(question % i === 0) return no;
-}
-}
-return yes; 
+  if (question <= 1) return no;
+  if (question === 2) return yes;
+
+  for (let i = 2; i < question; i += 1) {
+    if (question % i === 0) return no;
+  }
+
+  return yes;
 };
 
 const primeNumGame = () => {
   const maxNum = 25;
   const question = Math.floor(Math.random() * maxNum);
 
-  const trueAnswer = getAnswer(question);  
+  const trueAnswer = getAnswer(question);
 
   const arrQuestionAnswer = [question, trueAnswer];
 
