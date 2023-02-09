d1dc17596250f48af3d8f98077131e3a98dd0fd7
xyzelena <emkhuzhina@gmail.com>
2023-02-09 13:13:21 +0530

    Change  gcdGame.js. Use Euclidean algorithms with recursion to find gcd

--- src/games/gcdGame.js
+++ src/games/gcdGame.js
@@ -3,44 +3,16 @@ import { playGame } from '../index.js';
 
 const task = 'Find the greatest common divisor of given numbers.';
 
-const getDividersNum = (num) => {
-  const result = [];
-
-  if (num === 0 || num === 1) {
-    result.push(num);
-    return result;
-  }
-
-  for (let i = 0; i <= num; i += 1) {
-    if (num % i === 0) result.push(i);
-  }
-
-  return result;
-};
-
-const getGcd = (numbers_1, numbers_2) => {
-  const commonNumbers = [];
-
-  for (let i = 0; i < numbers_1.length; i += 1) {
-    if (numbers_2.includes(numbers_1[i])) commonNumbers.push(numbers_1[i]);
-  }
-
-  const result = commonNumbers[commonNumbers.length - 1];
-
-  return result;
-};
+const gcd = ( a,  b) => a === 0 ? b:gcd(b % a, a); 
 
 const gcdGame = () => {
-  const maxNum = 21; 
-  const num1 = getRandomNum(maxNum) + 1;
-  const num2 = getRandomNum(maxNum) + 1;
+  const maxNum = 21;
+  const num1 = getRandomNum(maxNum);
+  const num2 = getRandomNum(maxNum);
 
   const question = `${num1} ${num2}`;
 
-  const dividersNum_1 = getDividersNum(num1);
-  const dividersNum_2 = getDividersNum(num2);
-
-  const trueAnswer = String(getGcd(dividersNum_1, dividersNum_2));
+  const trueAnswer = String(gcd(num1, num2));
 
   return [question, trueAnswer];
 };
