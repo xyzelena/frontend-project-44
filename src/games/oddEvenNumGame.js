502f16c186b99cf19479575f7cd3f9e5743bb712
xyzelena <emkhuzhina@gmail.com>
2023-02-09 14:04:16 +0530

    Fix oddEvenNumGame.js with lint

--- src/games/oddEvenNumGame.js
+++ src/games/oddEvenNumGame.js
@@ -3,12 +3,12 @@ import playGame from '../index.js';
 
 const task = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-const isEven = (num) => (num % 2 === 0) ? true : false;
+const isEven = (num) => ((num % 2 === 0));
 
 const oddEvenNumGame = () => {
   const maxNum = 101;
   const question = getRandomNum(maxNum);
-  
+
   const trueAnswer = isEven(question) === true ? 'yes' : 'no';
 
   return [question, trueAnswer];
