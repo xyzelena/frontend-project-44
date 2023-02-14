52f4fb7b6eb1e4b9cb1768e129eb442b4de13819
xyzelena <emkhuzhina@gmail.com>
2023-02-15 00:14:50 +0530

    Fix  oddEvenNumGame.js

--- src/games/oddEvenNumGame.js
+++ src/games/oddEvenNumGame.js
@@ -3,13 +3,13 @@ import playGame from '../index.js';
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => num % 2 === 0;
+const isEven = (num) => (num % 2 === 0);
 
 const oddEvenNumGame = () => {
   const maxNum = 101;
   const question = getRandomNum(maxNum);
 
-  const trueAnswer = isEven(question) === true ? 'yes' : 'no';
+  const trueAnswer = isEven(question) ? 'yes' : 'no';
 
   return [question, trueAnswer];
 };
